1c8af872bf0e0a0f10ec2dfcd49130e699859fdb
Ercan ALTUN <salsel0147@gmail.com>
2022-09-06 17:51:23 +0300

    06/09

--- src/main/java/Jdbc_Practice/Query2.java
+++ src/main/java/Jdbc_Practice/Query2.java
@@ -1,9 +1,6 @@
 package Jdbc_Practice;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Query2 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -12,7 +9,15 @@ public class Query2 {
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed", "postgres", "Ss20112013Ss.");
         Statement st = con.createStatement();
 
-        System.out.println();
+        //Soru-1:Ögrenciler table daki Erkek öğrencileri listeleyiniz
+
+        ResultSet result1=st.executeQuery("Select * from ogrenciler Where cinsiyet='E'");
+
+        while (result1.next()) {
+
+            System.out.println(result1.getInt(1) + "==>" + result1.getString(2) + "==>" +
+                    result1.getString(3) + "==>" + result1.getString(4));
+        }
 
 
 
